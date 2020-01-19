abb75fadc7213df4c3c615d9c1028ce892ae21f8
Pratik <frek1546@gmail.com>
2020-01-20 00:16:48 +0530

    Preping for publishing schematic

--- Angular Schematics/order-wizard/src/order-wizard/files/test.ts
+++
@@ -1,8 +0,0 @@
-<%# This will not appear in the generated output %>
-// This will appear in the generated output
-
-const unescaped = '<%= "<" %>';
-const escaped = '<%- "<" %>';
-
-<% if(true) { %>const conditionalTest = true;<% } %>
-<% if(false) { %>const conditionalTest = false;<% } %>
